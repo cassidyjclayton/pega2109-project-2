c83799ddbbb198ed8ab4c315dd3ec3b36263b34b
cassidyjclayton <43220444+cassidyjclayton@users.noreply.github.com>
2021-11-10 19:09:24 -0500

    + q16 code

--- DemoProject2/src/primary/Q16.java
+++ DemoProject2/src/primary/Q16.java
@@ -4,3 +4,8 @@ Write a program to display the number of characters
 for a string input. The string should be entered as
 a command line argument using (String [ ] args).
 */
+public class Q16 {
+  public static void Count(String s) {
+    System.out.println(s.length());
+  }
+}
