6463bc21a94a67d6dbd4995591fb2a895282d8a3
cassidyjclayton <43220444+cassidyjclayton@users.noreply.github.com>
2021-11-10 19:48:29 -0500

    - old q18 file (empty); +  instructions to q18subclass; +q18superclass

---
+++ DemoProject2/src/primary/Q18Superclass.java
@@ -0,0 +1,7 @@
+package primary
+
+public abstract class Q18Superclass{
+  public abstract boolean hasUppercaseLetter(String s);
+  public abstract String toUpperCase(String s);
+  public abstract  int stringToInt(String s);
+}
